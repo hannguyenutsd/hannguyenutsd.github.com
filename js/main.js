06bb4ec9f1693a0d8b62fb9244cbd4937c972fd0
Thanh Nguyen <thanhnguyencs@utexas.edu>
2017-11-11 23:51:52 -0600

    Change the text of slider

--- js/main.js
+++ js/main.js
@@ -77,8 +77,8 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'img/slider-images/image01.jpg', title : '<div class="slide-content">UTHealth School of Dentistry</div>', thumb : '', url : ''},
-											{image : 'img/slider-images/class3.jpg', title : '<div class="slide-content">Class of 2020</div>', thumb : '', url : ''},
+											{image : 'img/slider-images/image01.jpg', title : '<div class="slide-content title-center">UTHealth School of Dentistry</div>', thumb : '', url : ''},
+											{image : 'img/slider-images/class3.jpg', title : '<div class="slide-content title-near-top">Class of 2020</div>', thumb : '', url : ''},
 											{image : 'img/slider-images/dental-care-2.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}
 											// {image : 'img/slider-images/image04.jpg', title : '<div class="slide-content">Brushed</div>', thumb : '', url : ''}  
 									],
